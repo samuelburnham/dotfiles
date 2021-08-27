a92b993341d46fdb29de0a6953a8127c611b97b1
samuelburnham <samuel.burnham@gmail.com>
2021-08-27 08:06:44 -0400

    Add org mode

--- init.el
+++ init.el
@@ -1,4 +1,3 @@
-
 ;; Initialize package sources
 (require 'package)
 
@@ -103,7 +102,7 @@
   "qq"  '(kill-emacs :which-key "quit")
 ;;  ;; NeoTree
 ;;  ;;"ft"  '(neotree-toggle :which-key "toggle neotree")
-;;  ;;"br"  '(rename-current-buffer-file :which-key "rename current buffer file")
+  ;;"br"  '(rename-current-buffer-file :which-key "rename current buffer file")
 ;;  ;; Org
 ;;  ;; Others
 ;;  "at"  '(eshell :which-key "open shell")
@@ -118,14 +117,22 @@
   (evil-mode 1))
 
 ;; Delete without register, "DD" deletes line
-  (evil-define-operator evil-delete-without-register (beg end type yank-handler)
-    (interactive "<R><y>")
-    (evil-delete beg end type ?_ yank-handler))
-  (define-key evil-normal-state-map (kbd "D") 'evil-delete-without-register)
-  (define-key evil-visual-state-map (kbd "D") 'evil-delete-without-register)
+(evil-define-operator evil-delete-without-register (beg end type yank-handler)
+  (interactive "<R><y>")
+  (evil-delete beg end type ?_ yank-handler))
+(define-key evil-normal-state-map (kbd "D") 'evil-delete-without-register)
+(define-key evil-visual-state-map (kbd "D") 'evil-delete-without-register)
 
 ;; Do something similar with 'evil-change "C-w" change without register/kill ring
 ;;(define-key evil-visual-state-map (kbd "c") 'evil-delete-without-register)
+
+;; Use visual line motions, allowing scroll within wrapped lines
+(evil-global-set-key 'motion "j" 'evil-next-visual-line)
+(evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+
+;; (evil-set-initial-state
+;;(use-package evil-collection)
+
 
 ;; Keep cursor in place when scrolling
 (setq scroll-preserve-screen-position 1)
@@ -149,6 +156,7 @@
 (global-set-key (kbd "M-k") 'scroll-down-line)
 (global-set-key (kbd "M-j") 'scroll-up-line)
 
+
 ;; Helm
 (use-package helm
   :init
@@ -168,27 +176,27 @@
         helm-autoresize-min-height 20)
   :config
   (helm-mode 1))
-
-;;(use-package ivy
-;;  :diminish
-;;  :bind (("C-s" . swiper)
-;;         :map ivy-minibuffer-map
-;;         ("TAB" . ivy-alt-done)       
-;;         ("C-l" . ivy-alt-done)
-;;         ("C-j" . ivy-next-line)
-;;         ("C-k" . ivy-previous-line)
-;;         :map ivy-switch-buffer-map
-;;         ("C-k" . ivy-previous-line)
-;;         ("C-l" . ivy-done)
-;;         ("C-d" . ivy-switch-buffer-kill)
-;;         :map ivy-reverse-i-search-map
-;;         ("C-k" . ivy-previous-line)
-;;         ("C-d" . ivy-reverse-i-search-kill))
-;;  :config
-;;  (ivy-mode 1))
-
-;;(use-package command-log-mode)
-
+;;
+;;;;(use-package ivy
+;;;;  :diminish
+;;;;  :bind (("C-s" . swiper)
+;;;;         :map ivy-minibuffer-map
+;;;;         ("TAB" . ivy-alt-done)       
+;;;;         ("C-l" . ivy-alt-done)
+;;;;         ("C-j" . ivy-next-line)
+;;;;         ("C-k" . ivy-previous-line)
+;;;;         :map ivy-switch-buffer-map
+;;;;         ("C-k" . ivy-previous-line)
+;;;;         ("C-l" . ivy-done)
+;;;;         ("C-d" . ivy-switch-buffer-kill)
+;;;;         :map ivy-reverse-i-search-map
+;;;;         ("C-k" . ivy-previous-line)
+;;;;         ("C-d" . ivy-reverse-i-search-kill))
+;;;;  :config
+;;;;  (ivy-mode 1))
+;;
+;;;;(use-package command-log-mode)
+;;
 ;; Which Key
 (use-package which-key
   :init
@@ -212,26 +220,76 @@
   :init (doom-modeline-mode 1)
   :custom ((doom-modeline-height 15)))
 
-;; Programming Configs
-;; Global tabs into two spaces unless otherwise specified
-(setq-default indent-tabs-mode nil)
-(setq-default tab-width 2)
-(setq tab-width 2)
-;;(defvaralias 'rust-indent-offset 'tab-width)
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+;;(use-package counsel
+;;  :bind(("M-x" . counsel-M-x))
+
+;;;; Programming Configs
+;;;; Global tabs into two spaces unless otherwise specified
+;;(setq-default indent-tabs-mode nil)
+;;(setq-default tab-width 2)
+;;;;(defvaralias 'rust-indent-offset 'tab-width)
+;;
+;;(use-package rustic
+;;  :ensure
+;;  :config
+;;  (setq rustic-indent-offset 2)
+;;  (setq rustic-format-on-save t)
+;;  (add-hook 'rustic-mode-hook rustic-mode-hook))
+;;
+;;(defun rustic-mode-hook ()
+;;  (when buffer-file-name
+;;    (setq-local buffer-save-without-query t)))
+
+(use-package rust-mode
+	:config
+	(setq rust-indent-offset 2)
+	(setq rust-format-on-save t))
+	
 
 ;;Keybindings for lambda and forall
 (global-set-key (kbd "M-l") "λ")
 (global-set-key (kbd "M-;") "∀")
 
-(use-package rainbow-delimiters
-  :hook (prog-mode . rainbow-delimiters-mode))
+;; (setq lean4-mode-required-packages '(dash f flycheck lsp-mode magit-section s))
+;; (use-package lean4-mode)
+;;(use-package company-lean)
+;; (use-package helm-lean)
 
-(use-package counsel
-  :bind(("M-x" . counsel-M-x))
+(global-set-key (kbd "S-SPC") #'company-complete)
 
+;; Org mode -------------------------------------------------
+(defun org-mode-setup ()
+  (org-indent-mode)
+  (visual-line-mode 1)
+  (font-lock-add-keywords 'org-mode
+                          '(("^ *\\([-]\\) "
+                             (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•")))))))
+  
+(use-package org
+  :hook (org-mode . org-mode-setup)
+  :config
+  (setq org-ellipsis " ⬎"))
 
+(use-package org-bullets
+  :after org
+  :hook (org-mode . org-bullets-mode)
+  :custom
+  (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
 
-        
+;; Set faces for heading levels
+;;(dolist (face '((org-level-1 . 1.2)
+;;                (org-level-2 . 1.1)
+;;                (org-level-3 . 1.05)
+;;                (org-level-4 . 1.0)
+;;                (org-level-5 . 1.1)
+;;                (org-level-6 . 1.1)
+;;                (org-level-7 . 1.1)
+;;                (org-level-8 . 1.1)))
+;;  (set-face-attribute (car face) nil :font "Cantarell" :weight 'regular :height (cdr face)))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -239,7 +297,7 @@
  ;; If there is more than one, they won't work right.
  '(helm-completion-style 'emacs)
  '(package-selected-packages
-   '(rainbow-delimiters ace-window which-key use-package typescript-mode tern spaceline smooth-scrolling rustic rust-mode neotree lsp-ui js2-mode ivy helm-rg helm-projectile general flycheck evil-org evil-escape eglot doom-themes doom-modeline company-lsp command-log-mode anzu)))
+   '(lean4-mode company-lean lean-mode rainbow-delimiters ace-window which-key use-package typescript-mode tern spaceline smooth-scrolling rustic rust-mode neotree lsp-ui js2-mode ivy helm-rg helm-projectile general flycheck evil-org evil-escape eglot doom-themes doom-modeline company-lsp command-log-mode anzu)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
